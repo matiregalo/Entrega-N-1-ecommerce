2f6d98a05aabd6855820151d3a19863758581ab0
matiregalo <matiasregalo64@gmail.com>
2025-11-21 14:24:18 -0300

    loginregisterform ok

---
+++ src/components/users/Login-RegisterForm/Login-RegisterForm.jsx
@@ -0,0 +1,32 @@
+
+const Login_RegisterForm = ({dataForm, handleChangeInput}) => {
+  return (
+        <div>
+            <div className="input-box">
+              <label htmlFor="email">Email</label>
+              <input className="input"
+                placeholder="Email"
+                type="email"
+                id="email"
+                name="email"
+                value={dataForm.email}
+                onChange={handleChangeInput}
+              />
+            </div>
+            <div className="input-box">
+              <label htmlFor="password">Contraseña</label>
+              <input
+                className="input"
+                placeholder="Contraseña"
+                type="password"
+                id="password"
+                name="password"
+                value={dataForm.password}
+                onChange={handleChangeInput}
+              />
+            </div>
+        </div>
+  )
+}
+
+export default Login_RegisterForm
